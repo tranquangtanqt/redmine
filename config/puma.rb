55b9028f9da36ff02931f1ba2036b5f53980d47c
tranquangtanqt <tranquangtanqt1990@gmail.com>
2023-08-02 22:22:30 +0700

    config deploy render

---
+++ config/puma.rb
@@ -0,0 +1,14 @@
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked web server processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+workers ENV.fetch("WEB_CONCURRENCY") { 4 } ### CHANGE: Un-comment out this line; update the value to 4
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+preload_app! ### CHANGE: Un-comment out this line
